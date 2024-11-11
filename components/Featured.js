98a49a7c4edbce279273e51d841efaa104ef5f0a
“tuananhkim555” <tuananhkim555@gmail.com>
2024-11-11 18:22:06 +0700

    uploadnewday

--- components/Featured.js
+++ components/Featured.js
@@ -12,23 +12,6 @@ const Bg = styled.div`
   padding: 70px 0;
   position: relative;
   overflow: hidden;
-  
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: linear-gradient(45deg, rgba(0,0,0,0.2) 0%, rgba(0,0,0,0) 100%);
-    animation: gradientShift 8s ease infinite;
-  }
-
-  @keyframes gradientShift {
-    0% { opacity: 0.2; }
-    50% { opacity: 0.8; }
-    100% { opacity: 0.2; }
-  }
 `;
 
 const Title = styled.h1`
